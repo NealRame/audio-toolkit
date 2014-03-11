388de407fd9d4b68739a55266e6b08c470cc0e0b
NealRame <contact@nealrame.com>
2014-03-11 11:33:17 +0100

    Update 'sources/main.cpp'
    nothing very interesting … just for testing.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,40 +1,31 @@
+#include <algorithm>
 #include <iostream>
 
+#include <audio/buffer>
 #include <audio/format>
 #include <audio/frame>
 #include <audio/sequence>
 
+#include <audio/codec>
+
+#include <fstream>
+#include <stdio.h>
+
 using namespace com::nealrame;
 
-const unsigned int CHANNEL_COUNT = 2;
-const unsigned int SAMPLE_RATE   = 44100;
-
 int main(int argc, char **argv) {
-	char data[CHANNEL_COUNT*SAMPLE_RATE*sizeof(float)];
-	utils::static_buffer buf(data, sizeof(data));
-	audio::sequence seq(audio::format(CHANNEL_COUNT, SAMPLE_RATE), buf);
+	std::shared_ptr<audio::codec::decoder> decoder = audio::codec::decoder::get_decoder(".wav");
+	std::shared_ptr<audio::buffer> audio_buffer = decoder->decode("input.wav");
 
-	auto first = seq.begin();
+	audio::sequence seq = audio_buffer->sequence();
+	unsigned int count = 0;
 
-	for (audio::frame frame: seq) {
-		for (float &sample: frame) {
-			sample = 0;
-		}
-	}
+	std::cout << seq.raw_buffer().length() << std::endl;
+	std::cout << seq.frame_count() << std::endl;
 
-	for (auto seq_it = first, seq_end = seq.end();
-		seq_it != seq_end; ++seq_it) {
-
-		std::cout << (seq_it - first) << ": ";
-
-		audio::frame frame = *seq_it;
-		for (auto frame_it = frame.begin(), frame_end = frame.end();
-			frame_it != frame_end; ++frame_it) {
-			std::cout << *frame_it << ' ';
-		}
-
-		std::cout << std::endl;
-	}
+	std::for_each (seq.begin(), seq.end(), [&count](audio::frame f) {
+		std::cout << '[' << count++ <<"]: " << *(f.begin()) << std::endl;
+	});
 
 	return 0;
 }
