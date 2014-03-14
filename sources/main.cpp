5ccf070d334eca60f401f8dd231f68dfddc4922b
NealRame <contact@nealrame.com>
2014-03-14 02:21:53 +0100

    Test

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,6 +6,7 @@
 #include <audio/buffer>
 #include <audio/format>
 #include <audio/frame>
+#include <audio/generator>
 #include <audio/sequence>
 
 #include <audio/codec>
@@ -14,9 +15,16 @@ using namespace com::nealrame;
 
 int main(int argc, char **argv) {
 
-	std::shared_ptr<audio::codec::decoder> decoder = audio::codec::decoder::get(".wav");
-	std::shared_ptr<audio::buffer> audio_buffer = decoder->decode("input.wav");
+	// std::shared_ptr<audio::codec::decoder> decoder = audio::codec::decoder::get(".wav");
+	// std::shared_ptr<audio::buffer> audio_buffer = decoder->decode("input.wav");
 	
+	audio::format format(2, 44100);
+	// audio::generator<audio::noise_engine> noise(format);
+	audio::generator<audio::sine_engine> sine(format, 0.5, 55.);
+
+	// std::shared_ptr<audio::buffer> audio_buffer = noise.buffer(2.);
+	std::shared_ptr<audio::buffer> audio_buffer = sine.buffer(2.);
+
 	std::shared_ptr<audio::codec::coder> coder = audio::codec::coder::get(".wav");
 	coder->encode("output.wav", audio_buffer->sequence());
 	
