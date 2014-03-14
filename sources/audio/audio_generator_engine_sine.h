b6671be6de5705f50da03dfb44cc6b1d40eb8888
NealRame <contact@nealrame.com>
2014-03-15 00:23:01 +0100

    Update sine and noise generator classes
    - fix comment header
    - place method implementation in a separate cpp file

--- sources/audio/audio_generator_engine_sine.h
+++ sources/audio/audio_generator_engine_sine.h
@@ -1,59 +1,31 @@
 /*
- * audio_error.h
+ * audio_generator_engine_sine.h
  *
- * Created on: March 08, 2014
+ * Created on: March 13, 2014
  *     Author: [NealRame](mailto:contact@nealrame.com)
  */
 #ifndef AUDIO_SINE_GENERATOR_ENGINE_H_
 #define AUDIO_SINE_GENERATOR_ENGINE_H_
 
-#include <cmath>
-
-#include <boost/math/constants/constants.hpp>
+#include <memory>
 
 #include <audio/format>
 #include <audio/frame>
-#include <audio/buffer>
-
-#include <iostream>
 
 namespace com {
 namespace nealrame {
 namespace audio {
-struct sine_engine {
-	sine_engine (class format fmt, float amplitude, float frequency) :
-		format(fmt),
-		buffer(fmt, 1/frequency) {
-
-		using namespace boost::math;
-
-		unsigned int sample_rate = fmt.sample_rate();
-		unsigned int frame_index = 0;
-		float c = constants::two_pi<float>()*frequency/sample_rate;
-
-		for (auto frame: buffer.sequence()) {
-			float v = amplitude*sinf(frame_index*c);
-			for (float &sample: frame) {
-				sample = v;
-			}
-			frame_index++;
-		}
-		
-		itr_ = buffer.sequence().cbegin();
-		end_ = buffer.sequence().cend();
-	}
-	const_frame frame() {
-		const_frame f = *itr_++;
-		if (itr_ == end_) {
-			itr_ = buffer.sequence().cbegin();
-		}
-		return f;
-	}
+struct sine {
+	sine (class format fmt, float amplitude, float frequency);
+	virtual ~sine ();
+	void reset () { t = 0; }
+	const_frame frame ();
 	class format format;
-	class buffer buffer;
+	float amplitude;
+	float t;
 private:
-	sequence::const_iterator itr_;
-	sequence::const_iterator end_;
+	struct impl;
+	std::unique_ptr<impl> pimpl_;
 };
 } // namespace audio
 } // namespace nealrame
